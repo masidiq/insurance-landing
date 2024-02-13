1a098e54569003df25ae924943772ea93b3775af
Sidiq <makbarsidiq@gmail.com>
2024-02-13 14:35:25 +0700

    init vue latest

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>Link</h1>
+    <ol>
+      <li><a href="https://insurance-qa-1.travel-tech.net/Reservation/Insurance/Quotation">Insurance QA</a></li>
+      <li><a href="http://localhost:44366/Reservation/Insurance/Quotation">Insurance QA localhost</a></li>
+    </ol>
+    
+    
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
